52f4ef186b4250cdd19570ffea3930fd3e5a8e53
Ehi <ehiagheaigg@gmail.com>
2018-01-21 20:26:31 +0000

    Added Login, Signup and dashboard pages

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,21 @@ const router = express.Router();
 /* GET index page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Express'
+    title: 'Crypto Monitor'
   });
 });
+
+router.get('/signin', (req, res) =>{
+	res.render('signin',{
+		title: 'Signin to Crypto'
+	});
+});
+
+router.get('/signup', (req, res) =>{
+  res.render('signup',{
+    title: 'SignUp to Crypto'
+  });
+});
+
 
 export default router;
